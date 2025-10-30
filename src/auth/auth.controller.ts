4a1fda003594c61f99bce67c5b42c1ff03dd3f95
Hafiz-Muhammad-Furqan <hafizm.furqan123@gmail.com>
2025-10-30 23:44:46 +0500

    Add: profile route and middleware (Authentication complete)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UserService } from 'src/user/user.service';
 import { RegisterUserDto } from './dto/registerUser.dto';
 import { LoginUserDto } from './dto/loginUser.dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
   @Post('register')
   async register(@Body() registerUserDto: RegisterUserDto) {
@@ -17,6 +29,17 @@ export class AuthController {
   async login(@Body() loginUserDto: LoginUserDto) {
     try {
       const user = await this.authService.loginUser(loginUserDto);
+      return user;
+    } catch (error) {
+      return { message: error.message };
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  async getProfile(@Request() req) {
+    try {
+      const user = await this.userService.getUserByEmail(req.user);
       return user;
     } catch (error) {
       return { message: error.message };
